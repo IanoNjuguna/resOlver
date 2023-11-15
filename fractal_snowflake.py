eb367514e32c6d10fa0031b8bac46aafed4f71b3
IanoNjuguna <inc3099@gmail.com>
2023-11-15 23:17:39 +0300

    fractal_snowflake.py: create a fractal snowflake

---
+++ fractal_snowflake.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+
+def snowflake_side(length, levels):
+    if levels == 0:
+        forward(length)
+        return
+    
+    length /= 3.0
+    
+    snowflake_side(length, levels - 1)
+    left(60)
+    snowflake_side(length, levels - 1)
+    right(120)
+    snowflake_side(length, levels - 1)
+    left(60)
+    snowflake_side(length, levels - 1)
+
+def create_snowflake(sides, length):
+    colors = ["green", "blue", "red", "purple" ]
+    
+    for i in range(sides):
+        color(colors[i])
+        snowflake_side(length, sides)
+        right(360 / sides)
+
+create_snowflake(4, 200)
+
+mainloop()
